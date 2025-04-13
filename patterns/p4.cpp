c603823a2a92487a09e349872f4317280eee29b0
Suparna Dhar <suparnad806@gmail.com>
2025-04-13 23:16:06 +0600

    Add files via upload

---
+++ patterns/p4.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >>n;
+    for(int i=0;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            for(j=1;j<=n-i;j++){
+                cout << " ";
+            }
+    
+       for(j=0;j<=2*n-i+1;j++){
+        cout << "*";
+       }
+
+        for(j=1;j<=i;j++){
+                cout << " ";
+            }
+        }
+       cout << endl;
+    }
+}
