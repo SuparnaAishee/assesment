0c5dc42ca2d5057dff1f215d2814bbd979d73ac4
Suparna Dhar <suparnad806@gmail.com>
2025-04-14 18:58:56 +0200

    Update p4.js

--- oop/p4.js
+++ oop/p4.js
@@ -14,3 +14,38 @@ class Dog {
 
 const myDog = new Dog("Buddy", "Labrador", 5);
 myDog.bark(); 
+
+
+//  2. Class: Car
+class Car {
+  constructor(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+
+  start() {
+    console.log(`The ${this.brand} ${this.model} is starting.`);
+  }
+
+  getAge(currentYear) {
+    return currentYear - this.year;
+  }
+}
+
+const myCar = new Car('Toyota', 'Corolla', 2018);
+myCar.start();
+console.log(myCar.getAge(2025)); 
+
+//  3. Class: Box
+class Box {
+  constructor(length, width, height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+
+  volume() {
+    return this.length * this.width * this.height;
+  }
+}
